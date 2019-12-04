676d1996d1558ea7fb052898732d39106d98e684
islent <leoislent@gmail.com>
2019-12-04 23:37:08 +0800

    Package Setup

---
+++ src/AstroIC.jl
@@ -0,0 +1,12 @@
+module AstroIC
+
+_precompile_(true)
+
+using Unitful, UnitfulAstro
+using Distributions
+
+using PhysicalParticles
+
+
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+using Tests
+
+using Unitful, UnitfulAstro
+using AstroIO, PhysicalParticles
+
+using AstroIC
+
+@testset "Plummer sphere" begin
+    @test 1 == 1
+end
