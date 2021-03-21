086943f672aa906111d8f93f8e8b0483ddf28b94
islent <leoislent@gmail.com>
2021-03-21 16:52:31 +0800

    rm softlength [skip ci]

--- src/physics.jl
+++ src/physics.jl
@@ -5,14 +5,3 @@ end
 function vmean2(T::Number, mass::Number, units = uAstro)
     return uconvert(getuVel(units), 8 * Constant(units).k_B * T / pi / mass)
 end
-
-
-
-"""
-function softlength
-
-    return recommended softening length
-"""
-function softlength(V::Number, N::Int64)
-    return cbrt(V / N)
-end
